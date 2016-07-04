47da7c7f9d9588a4d7cc01e3c3cc71c6d1dc7624
Umang1996 <zheka_hockyman@ukr.net>
2016-07-04 21:55:01 +0300

    Switch+IonicPopup

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -1,37 +1,60 @@
-angular.module('Calculator',[])
-.controller('mainCtrl',function($scope, $state){
-  $scope.calculatePage = function () {
-    $state.go('calculatePage')
-};
-})
-.controller('calculatePageCtrl',function($scope){
+angular.module('Calculator', [])
+  .controller('mainCtrl', function ($scope, $state) {
+    $scope.calculatePage = function () {
+      $state.go('calculatePage')
+    };
+  })
+  .controller('calculatePageCtrl', function ($scope, $ionicPopup) {
 
-  $scope.data = {
-    first:'',
-    sign:'',
-    second:'',
-    result:''
-  };
+    $scope.data = {
+      first: '',
+      sign: '',
+      second: '',
+      result: ''
+    };
     $scope.calculate = function () {
-      if ($scope.data.sign == '+') {
-        $scope.data.result = $scope.data.first + $scope.data.second;
-        console.log($scope.data.result)
-      }
-      else if ($scope.sign == '-') {
-        $scope.data.result = $scope.data.first - $scope.data.second;
-        return $scope.data.result;
-      }
-      else if ($scope.sign == '*') {
-        $scope.data.result = $scope.data.first * $scope.data.second;
-        return $scope.data.result;
-      }
-      else if ($scope.sign == '/') {
-        $scope.data.result = $scope.data.first / $scope.data.second;
-        return $scope.data.result;
-      }
-      if ($scope.data.second=='0' && $scope.data.sign=='/'){
-        alert("На ноль делить нельзя");
+
+      switch ($scope.data.sign) {
+        case "+":
+          $scope.data.result = $scope.data.first + $scope.data.second;
+          alert($scope.data.result);
+          break;
+
+
+        case "-":
+          $scope.data.result = $scope.data.first - $scope.data.second;
+          alert($scope.data.result);
+          break;
+
+        case "/":
+          $scope.data.result = $scope.data.first / $scope.data.second;
+
+
+          if ($scope.data.second == '0' && $scope.data.sign == '/') {
+
+            alert("На ноль делить нельзя");
+          }
+          // $scope.showAlert = function() {
+          //   $ionicPopup.alert({
+          //     title: 'Эй, Миша',
+          //     template: 'ты урод'
+          //   });
+          //
+          // }
+
+
+
+
+          else {
+            alert($scope.data.result);
+          }
+          break;
+
+        case "*":
+          $scope.data.result = $scope.data.first * $scope.data.second;
+          alert($scope.data.result);
+          break;
+
       }
     }
-
-})
+  })
