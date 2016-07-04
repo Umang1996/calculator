216f488f262371a99337a0c445ee83ba8421b61a
romjkap <rtimashov1@gmail.com>
2016-07-04 19:07:17 +0300

    Привязать Js к  Calculation Screen Действия : Умножить, поделить, сумма, разница (переделано 4й раз)

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -1,38 +1,36 @@
-angular.module('Calculator',[])
-.controller('mainCtrl',function($scope, $state){
-  $scope.calculatePage = function () {
-    $state.go('calculatePage')
-};
-})
-.controller('calculatePageCtrl',function($scope){
-
-  $scope.calculate = function($scope){
-    $scope.firstNum = '';
-    $scope.secondNum = '';
-    $scope.variables = {
-      first:'',
-      sign:'',
-      second:''
+angular.module('Calculator', [])
+  .controller('mainCtrl', function ($scope, $state) {
+    $scope.calculatePage = function () {
+      $state.go('calculatePage')
     };
-    $scope.variables.first = $scope.firstNum;
-    $scope.variables.sign = $scope.sign;
-    $scope.variables.second = $scope.secondNum;
-    if($scope.sign == '+'){
-      $scope.result = $scope.variables.first + $scope.variables.second;
-      return $scope.result;
+  })
+
+  .controller('calculatePageCtrl', function ($scope) {
+
+  $scope.data = {
+    first:'',
+    sign:'',
+    second:'',
+    result:''
+  };
+    $scope.calculate = function () {
+      if ($scope.data.sign == '+') {
+        $scope.data.result = $scope.data.first + $scope.data.second;
+        console.log($scope.data.result)
+      }
+      else if ($scope.sign == '-') {
+        $scope.result = $scope.data.first - $scope.data.second;
+        return $scope.result;
+      }
+      else if ($scope.sign == '*') {
+        $scope.result = $scope.data.first * $scope.data.second;
+        return $scope.result;
+      }
+      else if ($scope.sign == '/') {
+        $scope.result = $scope.data.first / $scope.data.second;
+        return $scope.result;
+      }
 
     }
-    else if($scope.sign == '-'){
-      $scope.result = $scope.variables.first - $scope.variables.second;
-      return $scope.result;
-    }
-    else if($scope.sign == '*'){
-      $scope.result = $scope.variables.first * $scope.variables.second;
-      return $scope.result;
-    }
-    else if($scope.sign == '/'){
-      $scope.result = $scope.variables.first / $scope.variables.second;
-      return $scope.result;
-    }
-  }
-});
+
+})
